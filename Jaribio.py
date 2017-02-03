30ac8328642767d8af39af17782681d58ef5ca42
jarhill0 <jarhill0@gmail.com>
2017-02-03 13:46:37 -0800

    rewrote is_user_deleted function

--- Jaribio.py
+++ Jaribio.py
@@ -37,9 +37,7 @@ reddit = praw.Reddit(
 # function sets a flair to test if a user exists or not
 def is_user_deleted(user):
     try:
-        reddit.subreddit('Jaribio2').flair.set(redditor=user.strip(),
-                                               text='test',
-                                               css_class='')
+        reddit.redditor(user.strip()).fullname
     except:
         return True
     else:
