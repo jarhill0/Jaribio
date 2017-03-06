fff37f990797ea2b3d212d5044ab12a00905bc23
jarhill0 <jarhill0@gmail.com>
2017-03-06 13:20:42 -0800

    Experimentally combined functionality of Redefinition.py and Jaribio.py; added a config.py file

--- Redefinition.py
+++ Redefinition.py
@@ -3,17 +3,26 @@ import os
 import time
 import prawcore
 import praw
+from config import testing, target_sub, style
+import post_gist
+from update_sidebar import update_sidebar
 
-# Configuration
-target_sub = 'Redefinition'
-testing = False
+# Configuration double-check
+print('Target sub is %s.' % target_sub)
+print('Testing mode.' if testing else 'Real mode.')
+print('Style is %s.' % style)
+
 # set a time string for file saving and access; find now and a week ago in seconds since epoch
 local_time = time.localtime()
 time_string = time.strftime('%Y-%m-%d', local_time)
 now = time.time()
 week_ago = now - 604800
+
 # define selftext
-selftext = 'Kicked users:\n\n'
+if style == 'Redefinition':
+    selftext = 'Kicked users:\n\n'
+elif style == 'Jaribio':
+    selftext = '#Users removed\n\n'
 
 
 # opens, reads, and returns a resource
@@ -26,7 +35,7 @@ def read_resource(resource_filename):
 total_user_logs = int(read_resource('TotalUserLogs.txt'))
 
 # log in to Reddit
-reddit = praw.Reddit('Redefinition', user_agent='Private Sub Manager')
+reddit = praw.Reddit(target_sub, user_agent='Private Sub Manager')
 
 
 # function gets a user's fullname to test if a user exists or not
@@ -100,7 +109,11 @@ with open(os.path.abspath('Removed %s.txt' % time_string), 'w+') as f:
         f.write(user.strip() + '\n')
 # update selftext with kicked users and their numbers *before* they are removed from the user list.
 for user in to_remove:
-    selftext += ('- \\#%s /u/%s  \n' % (str((user_list.index(user)) + 1), user.strip()))
+    if style == 'Redefinition':
+        selftext += ('- \\#%s /u/%s  \n' % (str((user_list.index(user)) + 1), user.strip()))
+    elif style == 'Jaribio':
+        selftext += ('\\#%s - /u/%s  \n' % (str((user_list.index(user)) + 1), user.strip()))
+
 # remove users, THE RIGHT WAY, without skipping over some of them randomly
 user_list_copy = user_list[:]
 for user in user_list_copy:
@@ -112,10 +125,38 @@ for user in user_list_copy:
 def flair_existing_users():
     for i, user in enumerate(user_list):
         if not testing:
-            reddit.subreddit(target_sub).flair.set(
-                redditor=user.strip(),
-                text='#%d' % (i + 1),
-                css_class='number')
+
+            if style == 'Redefinition':
+                reddit.subreddit(target_sub).flair.set(
+                    redditor=user.strip(),
+                    text='#%d' % (i + 1),
+                    css_class='number')
+
+            elif style == 'Jaribio':
+                if i == 0:
+                    reddit.subreddit(target_sub).flair.set(  # commentOutToTest
+                        redditor=user.strip(),
+                        text='#1',
+                        css_class='goldnumber')
+                    print('Flaired %s.' % user)
+                elif i == 1:
+                    reddit.subreddit(target_sub).flair.set(  # commentOutToTest
+                        redditor=user.strip(),
+                        text='#2',
+                        css_class='silver')
+                    print('Flaired %s.' % user)
+                elif i == 2:
+                    reddit.subreddit(target_sub).flair.set(  # commentOutToTest
+                        redditor=user.strip(),
+                        text='#3',
+                        css_class='bronze')
+                    print('Flaired %s.' % user)
+                else:
+                    reddit.subreddit(target_sub).flair.set(  # commentOutToTest
+                        redditor=user.strip(),
+                        text='#%d' % (i + 1),
+                        css_class='number')
+
         print('Flaired %s.' % user)
 
 
@@ -126,16 +167,24 @@ num_to_add = max(min(len(to_remove), 25), 10)
 
 # get 30 comments and put their authors in RawNewComments
 raw_new_comment_authors = []
+if style == 'Jaribio':
+    raw_new_comment_urls = []
 for comment in reddit.subreddit('all').comments(limit=40):
     raw_new_comment_authors.append(comment.author.name)
+    if style == 'Jaribio':
+        raw_new_comment_urls.append('https://reddit.com' + comment.permalink(fast=True))
 
 # open a file; add users from raw_new_comments if we still need users and they are not already in user_list
+if style == 'Jaribio':
+    comments_for_entry = []
 with open('New users %s.txt' % time_string, 'w+') as f:
     n = 0
     for i, user in enumerate(raw_new_comment_authors):
         if n < num_to_add and not user_list.__contains__(user.strip()):
             # open the new user file and read it line by line
             f.write(str(user.strip()) + '\n')
+            if style == 'Jaribio':
+                comments_for_entry.append(raw_new_comment_urls[i])
             n += 1
 
 new_users = open('New users %s.txt' % time_string, 'r').readlines()
@@ -144,29 +193,54 @@ for i, user in enumerate(new_users):
 if new_users[-1] == '':
     del new_users[-1]
 
+if style == 'Jaribio':
+    gist_body = '#Comments for entry on %s\n\n' % time_string
+    for i, user in enumerate(new_users):
+        gist_body += '%s : %s  \n' % (user, comments_for_entry[i])
+    gist_url = post_gist.make_gist(gist_body, time_string)
+
 num_old_users = len(user_list)
 # add the new users to selftext, then post it.
-selftext += '\nAdded users:\n\n'
+if style == 'Redefinition':
+    selftext += '\nAdded users:\n\n'
+elif style == 'Jaribio':
+    selftext += '\n#Users added\n\n[Comments for entry](%s)\n\n' % gist_url
 for i, user in enumerate(new_users):
-    selftext += ('- \\#%s /u/%s  \n' % (str(num_old_users + i + 1), user.strip()))
+    if style == 'Redefinition':
+        selftext += ('- \\#%s /u/%s  \n' % (str(num_old_users + i + 1), user.strip()))
+    elif style == 'Jaribio':
+        selftext += ('\\#%s - /u/%s  \n' % (str(num_old_users + i + 1), user.strip()))
 
 # add stats section and a link to the source code at the bottom of the post
-num_users_change = ''
-if len(new_users) - len(to_remove) >= 0:
-    num_users_change = '+%s' % str(len(new_users) - len(to_remove))
-else:
-    num_users_change = str(len(new_users) - len(to_remove))
-selftext += ('\nInfo:\n\n- %s users kicked\n- %s users added\n- Membercap: %s (%s)' % (
-    str(len(to_remove)), str(len(new_users)), str(len(user_list) + len(new_users)), num_users_change))
+if style == 'Redefinition':
+    num_users_change = ''
+    if len(new_users) - len(to_remove) >= 0:
+        num_users_change = '+%s' % str(len(new_users) - len(to_remove))
+    else:
+        num_users_change = str(len(new_users) - len(to_remove))
+    selftext += ('\nInfo:\n\n- %s users kicked\n- %s users added\n- Membercap: %s (%s)' % (
+        str(len(to_remove)), str(len(new_users)), str(len(user_list) + len(new_users)), num_users_change))
 
 if not testing:
-    new_post = reddit.subreddit(target_sub).submit(time_string + ' - Bot Recap', selftext=selftext, resubmit=False)
+    if style == 'Redefinition':
+        new_post = reddit.subreddit(target_sub).submit(
+            time_string + ' - Bot Recap',
+            selftext=selftext,
+            resubmit=False)
+    elif style == 'Jaribio':
+        new_post = reddit.subreddit(target_sub).submit(
+            'Jaribio user log #%s' % str(total_user_logs + 1),
+            selftext=selftext,
+            resubmit=False)
 print('Submitted')
 print(selftext)
-# distinguish it
+# distinguish/sticky it
 if not testing:
     reddit.submission(id=new_post.id).mod.distinguish(how='yes', sticky=True)
 print('Distinguished')
+if style == 'Jaribio':
+    reddit.submission(id=new_post.id).mod.sticky()
+    print('Stickied')
 
 # after posting, increment the total number of user logs
 with open('Resources/TotalUserLogs.txt', 'w+') as f:
@@ -192,3 +266,8 @@ with open('UserList.txt', 'w+') as f:
 with open('UserList.txt', 'a+') as f:
     for user in new_users:
         f.write(user.strip() + '\n')
+
+# must be called after writing out UserList.txt, because it pulls the user list from there.
+if style == 'Jaribio' and not testing:
+    update_sidebar(target_sub)
+
---
+++ config.py
@@ -0,0 +1,8 @@
+# Subreddit to activate bot on
+target_sub = 'Redefinition'
+
+# set to True to enable testing mode
+testing = True
+
+# set a style (choices: 'Redefinition', 'Jaribio')
+style = 'Redefinition'
